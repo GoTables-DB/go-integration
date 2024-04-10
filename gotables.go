580f2eb843cd89f29cfb20f34e5273afb9f11a9e
Jeroen Leuenberger <jereileu@proton.me>
2024-04-10 21:34:32 +0200

    Add RunServer() and TestServer() functions + add Config struct
    There are now two new functions for running a gotables server and testing the connection. Additionally, the information for the server and the connection are now in a config struct. Note: The RunServer() function can only run servers locally. Setting the Host field in Config to something external will result in failure when testing the connection if the server was started using this module.

--- gotables.go
+++ gotables.go
@@ -4,31 +4,37 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"git.jereileu.ch/gotables/server/gt-server/fs"
 	"git.jereileu.ch/gotables/server/gt-server/server"
 	"git.jereileu.ch/gotables/server/gt-server/table"
 	"io"
 	"net/http"
 )
 
+type Config struct {
+	fs.Conf
+	Host string
+}
 type requestBody = server.Body
 type Column = table.Column
 type Table = table.Table
 type TableU = table.TableU
 
-func ConstructUrl(tbl, db, host, port, protocol string) (string, error) {
+func ConstructUrl(tbl string, db string, config Config) (string, error) {
 	out := ""
-	if protocol != "" {
-		out += protocol
-		out += "://"
+	if config.HTTPSMode {
+		out += "https://"
+	} else {
+		out += "http://"
 	}
-	if host != "" {
-		out += host
+	if config.Host != "" {
+		out += config.Host
 	} else {
 		return "", errors.New("invalid url: host needs to be set")
 	}
-	if port != "" {
+	if config.Port != "" {
 		out += ":"
-		out += port
+		out += config.Port
 	}
 	if tbl != "" {
 		if db != "" {
@@ -73,4 +79,17 @@ func DoRequest(req *http.Request) (Table, error) {
 	}
 	tbl, err := tblU.ToT()
 	return tbl, err
+}
+
+func RunServer(config Config) {
+	server.Run(config.Conf)
+}
+
+func TestServer(config Config) error {
+	url, err := ConstructUrl("", "", config)
+	if err != nil {
+		return err
+	}
+	_, err = http.Get(url)
+	return err
 }
